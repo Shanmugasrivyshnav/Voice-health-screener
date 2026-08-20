03185fdd7c14f9e1c6e3c69c2a6f69e7345063be
SHANMUGA SREE VYSHNAV <Shanmukhasri00@gmail.com>
2026-08-20 14:30:23 +0530

    Update env.js

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -7,7 +7,7 @@ export const env = {
     process.env.CLIENT_ORIGIN ||
     "https://voice-health-screener-three.vercel.app",
 
-  groqApiKey: process.env.GROQ_API_KEY || "",
+  groqApiKey: process.env.GROQ_API_KEY || "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",
 
   groqModel: process.env.GROQ_MODEL || "llama-3.3-70b-versatile",
 };
